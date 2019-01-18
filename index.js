b93662841f20cd807b85ac7dce4d06467bb01297
Glitch (agile-boy) <none>
2019-01-18 02:35:46 +0000

    Add cron scheduler to send weekly email

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const app = require("./server.js");
+const {runWeekly} = require("./scheduler.js")
+const agileMailer = require("./agileMailer")
+
+//Send an agile email to subscribers every Monday morning
+runWeekly(1, 8, 0, agileMailer)
 
 
-// listen for requests :)
+//Start the web server
 const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
